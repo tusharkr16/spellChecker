917d1da985123526857150d53bcb8da13ee328a9
tusharkr16 <tusharkumar9871@gmail.com>
2024-07-02 01:50:50 +0530

    Initialize project using Create React App

---
+++ src/Spell.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+
+const customDictionary = {
+    teh: "the",
+    wrok: "work",
+    fot: "for",
+    exampl: "example"
+};
+
+const Spell = () => {
+    return (
+        <div>Spell</div>
+    )
+}
+
+export default Spell
